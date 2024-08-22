47856a108990a96d1310f5796a0c1a26583062da
Mikhail Belskiy <mikl.belskiy@yandex.ru>
2024-08-22 21:53:14 +0300

    wome_work_3

--- HomeWork_file.py
+++ HomeWork_file.py
@@ -1,5 +1,6 @@
 
 from pprint import pprint
+import os
 
 cook_book = {}
 with open('/home/mikl/Рабочий стол/homework/recipes.txt', 'rt', encoding = "utf-8") as file:
@@ -32,4 +33,42 @@ def get_shop_list_by_dishes (dishes:list, person_count: int):
             print(f'Блюда "{dish}" нет в книге рецептов')
     return ingridients
 
-pprint (get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+#pprint (get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+
+
+
+with open('/home/mikl/Рабочий стол/homework/HW_files/1.txt', 'rt', encoding = "utf-8") as f1, open('/home/mikl/Рабочий стол/homework/HW_files/2.txt', 'rt', encoding = "utf-8") as f2, open('/home/mikl/Рабочий стол/homework/HW_files/3.txt', 'rt', encoding = "utf-8") as f3, open('/home/mikl/Рабочий стол/homework/HW_files/answer.txt', 'a+', encoding = "utf-8") as f4:
+
+    line1 = f1.readlines()
+    len_line_1 = len(line1)
+    file_path_1 = '/home/mikl/Рабочий стол/homework/HW_files/1.txt'
+    file_name_1 = os.path.basename(file_path_1)
+
+    line2 = f2.readlines()
+    len_line_2 = len(line2)
+    file_path_2 = '/home/mikl/Рабочий стол/homework/HW_files/2.txt'
+    file_name_2 = os.path.basename(file_path_2)
+
+    line3 = f3.readlines()
+    len_line_3 = len(line3)
+    file_path_3 = '/home/mikl/Рабочий стол/homework/HW_files/3.txt'
+    file_name_3 = os.path.basename(file_path_3)
+
+    dict_ = {len_line_1: [line1, file_name_1], len_line_2:[line2, file_name_2], len_line_3: [line3, file_name_3]}
+    sort_list = dict(sorted(dict_.items()))
+
+
+    for key, value in sort_list.items():  #хочу яерез цикл из словаря записать в файл решение, добавив через basename() 
+        f4.write(f'\n{key}\n')
+        f4.write(f'{value[1]} \n')
+        f4.write(f'{"".join(value[0])}')
+#print(sort_list)
+print(f4)
+
+
+
+
+
+
+
